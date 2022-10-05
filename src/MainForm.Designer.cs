b4271e208a54b21832aed57972a0f8c815e8f2ca
Denys Shevel <denys.shevel16@gmail.com>
2022-10-05 17:35:53 +0200

    Add a toolbar and a grid view

--- src/MainForm.Designer.cs
+++ src/MainForm.Designer.cs
@@ -29,13 +29,126 @@ namespace SharpTables
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.mainGridView = new System.Windows.Forms.DataGridView();
+            this.mainMenuStrip = new System.Windows.Forms.MenuStrip();
+            this.fileMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newWorkbookMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openWorkbookMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveAsMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.undoMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.redoMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.mainGridView)).BeginInit();
+            this.mainMenuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // mainGridView
+            // 
+            this.mainGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.mainGridView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.mainGridView.Location = new System.Drawing.Point(0, 24);
+            this.mainGridView.Name = "mainGridView";
+            this.mainGridView.RowTemplate.Height = 25;
+            this.mainGridView.Size = new System.Drawing.Size(916, 400);
+            this.mainGridView.TabIndex = 1;
+            // 
+            // mainMenuStrip
+            // 
+            this.mainMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileMenuItem,
+            this.editMenuItem});
+            this.mainMenuStrip.Location = new System.Drawing.Point(0, 0);
+            this.mainMenuStrip.Name = "mainMenuStrip";
+            this.mainMenuStrip.Size = new System.Drawing.Size(916, 24);
+            this.mainMenuStrip.TabIndex = 2;
+            this.mainMenuStrip.Text = "menuStrip1";
+            // 
+            // fileMenuItem
+            // 
+            this.fileMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newWorkbookMenuItem,
+            this.openWorkbookMenuItem,
+            this.saveMenuItem,
+            this.saveAsMenuItem});
+            this.fileMenuItem.Name = "fileMenuItem";
+            this.fileMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileMenuItem.Text = "File";
+            // 
+            // newWorkbookMenuItem
+            // 
+            this.newWorkbookMenuItem.Name = "newWorkbookMenuItem";
+            this.newWorkbookMenuItem.Size = new System.Drawing.Size(159, 22);
+            this.newWorkbookMenuItem.Text = "New workbook";
+            // 
+            // openWorkbookMenuItem
+            // 
+            this.openWorkbookMenuItem.Name = "openWorkbookMenuItem";
+            this.openWorkbookMenuItem.Size = new System.Drawing.Size(159, 22);
+            this.openWorkbookMenuItem.Text = "Open workbook";
+            // 
+            // saveMenuItem
+            // 
+            this.saveMenuItem.Name = "saveMenuItem";
+            this.saveMenuItem.Size = new System.Drawing.Size(159, 22);
+            this.saveMenuItem.Text = "Save";
+            // 
+            // saveAsMenuItem
+            // 
+            this.saveAsMenuItem.Name = "saveAsMenuItem";
+            this.saveAsMenuItem.Size = new System.Drawing.Size(159, 22);
+            this.saveAsMenuItem.Text = "Save as";
+            // 
+            // editMenuItem
+            // 
+            this.editMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.undoMenuItem,
+            this.redoMenuItem});
+            this.editMenuItem.Name = "editMenuItem";
+            this.editMenuItem.Size = new System.Drawing.Size(39, 20);
+            this.editMenuItem.Text = "Edit";
+            // 
+            // undoMenuItem
+            // 
+            this.undoMenuItem.Name = "undoMenuItem";
+            this.undoMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.undoMenuItem.Text = "Undo";
+            // 
+            // redoMenuItem
+            // 
+            this.redoMenuItem.Name = "redoMenuItem";
+            this.redoMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.redoMenuItem.Text = "Redo";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(916, 424);
+            this.Controls.Add(this.mainGridView);
+            this.Controls.Add(this.mainMenuStrip);
+            this.MainMenuStrip = this.mainMenuStrip;
+            this.Name = "MainForm";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.mainGridView)).EndInit();
+            this.mainMenuStrip.ResumeLayout(false);
+            this.mainMenuStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+        private System.Windows.Forms.DataGridView mainGridView;
+        private System.Windows.Forms.MenuStrip mainMenuStrip;
+        private System.Windows.Forms.ToolStripMenuItem fileMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newWorkbookMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openWorkbookMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveAsMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem editMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem undoMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem redoMenuItem;
     }
 }
 
