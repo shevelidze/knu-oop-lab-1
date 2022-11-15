a693c5badef4aebd0ec1873bc6436d8436b6e331
Denys Shevel <denys.shevel16@gmail.com>
2022-11-15 13:24:04 +0100

    Add a token class

---
+++ src/Table.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpTables
+{
+    class Table
+    {
+        Dictionary<string, string> setCellExpression(int cellRow, int cellColumn, string expression)
+        {
+            return new Dictionary<string, string>();
+        }
+    }
+}
---
+++ src/Token.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpTables
+{
+    enum TokenType
+    {
+        NumberValue,
+        BooleanValue,
+        StringValue,
+        Operator,
+        CellReference,
+        FunctionName
+    }
+
+    class Token
+    {
+        Token(string exrpession, int tokenBegin, int tokenEnd, TokenType type)
+        {
+            this.Expression = exrpession;
+            this.TokenBegin = tokenBegin;
+            this.TokenEnd = tokenEnd;
+            this.TokenType = type;
+        }
+        public string Expression { get; private set; }
+        public string Value {
+            get
+            {
+                return this.Expression.Substring(TokenBegin, TokenEnd - TokenBegin);
+            }
+        }
+        public int TokenBegin { get; private set; }
+        public int TokenEnd { get; private set; }
+        public TokenType TokenType { get; private set; }
+    }
+}
